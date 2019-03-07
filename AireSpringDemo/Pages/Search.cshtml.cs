bcf5a13c9dbbc3ae1c0a75b20cacec4e7e040876
yonathanseleshi <yonathan.seleshi@gmail.com>
2019-03-07 15:16:54 -0800

    Updating comments

--- AireSpringDemo/Pages/Search.cshtml.cs
+++ AireSpringDemo/Pages/Search.cshtml.cs
@@ -13,26 +13,33 @@ namespace AireSpringDemo.Pages
 {
     public class SearchModel : PageModel
     {
+        
+        //Getting a reference to the Database Context and the Repository
         private EmployeeDbContext _dbContext;
         private IEmployeesRepo _repo;
         
         
         
        
-       
+        //Initializing an IEnumerable of employees which is used to store the employees retrieved from the database
         IEnumerable<Employee> employees = new List<Employee>();
         
         
+        
+        //Binding the SearchString property to the search input
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
         
 
+      
         public IEnumerable<Employee> Employees
         {
             get => employees;
             set => employees = value;
         }
 
+        
+        
         public SearchModel(EmployeeDbContext context, IEmployeesRepo repo)
         {
             this._dbContext = context;
@@ -45,6 +52,10 @@ namespace AireSpringDemo.Pages
         public Employee Employee{ get; set; }
       
         
+        //OnGetAsync is called upon loading the Search Page
+        //All employees stored from the database are retrieved unless the Search box has contents
+        //If the search box has input is bound to the SearchString variable and employees with a first name
+        //matching the search string are retrieved
         public async Task OnGetAsync()
         {
           

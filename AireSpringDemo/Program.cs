8206256b2a70e32124e34966457cb9c984973e37
yonathanseleshi <yonathan.seleshi@gmail.com>
2019-03-07 10:25:40 -0800

    Setup IIS, Kestrel for Elastic Beanstalk Deployment

--- AireSpringDemo/Program.cs
+++ AireSpringDemo/Program.cs
@@ -19,6 +19,9 @@ namespace AireSpringDemo
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>();
     }
 }
